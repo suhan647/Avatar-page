f999080b1b011b8ca39fb084c65b23e67d60977c
Suhan Ahmed <suhanahmed647@gmail.com>
2023-04-26 09:10:54 +0000

    styled product details page

--- avatars/src/components/avatars/AvatarDetails.js
+++ avatars/src/components/avatars/AvatarDetails.js
@@ -1,9 +1,17 @@
 import { useState } from 'react';
 import Data from '../data/Data.json';
 import { useParams } from 'react-router-dom';
+import {
+  Typography,
+  Box,
+  Grid,
+  Paper,
+  Rating,
+  Button,
+  Divider,
+} from '@mui/material';
 
 const AvatarDetails = () => {
-  const [detail, setDetail] = useState([]);
 
   const { id } = useParams();
 
@@ -11,19 +19,63 @@ const AvatarDetails = () => {
 
   const avatar = Data.Avatars.find((a) => a.id === Number(id));
 
-
   console.log(avatar);
 
   return (
-    <>
+    <Box sx={{ p: 3 }}>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          mb: 3,
+        }}
+      >
+        <Typography variant="h4">{avatar.name}</Typography>
+        <Button variant="outlined" color="primary">
+          Back
+        </Button>
+      </Box>
 
-      {avatar && (
-        <>
-          <img src={avatar.image} alt={avatar.name} />
-          <p>{avatar.name}</p>
-        </>
-      )}
-    </>
+      <Grid container spacing={4}>
+        <Grid item xs={12} md={6}>
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <img src={avatar.image} alt={avatar.name} style={{ width: '80%' }} />
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+            <img src={avatar.image} alt={avatar.name} style={{ width: '20%' }} />
+            <img src={avatar.image} alt={avatar.name} style={{ width: '20%', ml: 2 }} />
+            <img src={avatar.image} alt={avatar.name} style={{ width: '20%', ml: 2 }} />
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Paper sx={{ p: 2 }}>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Rating name="avatar-rating" value={4.7} precision={0.1} readOnly />
+              <Typography variant="body2" sx={{ ml: 1 }}>
+                {avatar.starsandLikes}
+              </Typography>
+            </Box>
+            <Typography variant="h5" sx={{ my: 2 }}>
+              {avatar.name}
+            </Typography>
+            <Typography variant="body1" sx={{ my: 2 }}>
+              {avatar.description}
+            </Typography>
+            <Typography variant="body2" sx={{ my: 2 }}>
+              {avatar.smallName} - {avatar.system}
+            </Typography>
+            <Divider sx={{ my: 2 }} />
+            <Typography variant="h6" sx={{ my: 2 }}>
+              ${avatar.price}
+            </Typography>
+            <Button variant="contained" color="primary" sx={{ mt: 3 }}>
+              Add to Cart
+            </Button>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
